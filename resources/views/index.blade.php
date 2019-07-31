7c6c6cc7774faa68812ebabff55ab5a2fc554c3a
sungminlim89 <camelmevius@gmail.com>
2019-07-31 23:51:31 +0900

    test commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>Sungmin</title>
+        <style>
+            iframe {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+            }
+        </style>
+    </head>
+    <body>
+        <iframe width="1280" height="720" src="https://www.youtube.com/embed/6JtgGnAv6lA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+    </body>
+</html>
